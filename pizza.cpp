57e9e9933349f029e8d0c08827314ed2e5a5a7aa
Ashley Moy <mac@Ashleys-MacBook-Pro.local>
2020-06-01 10:01:03 -0400

    First commit

---
+++ pizza.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int main() 
+{
+    int slices; // declare variable
+    cout << "How many slices of pizza do you eat?" << std::endl;
+    cin >> slices;
+    cout << "You have " << slices << " slices of pizza." << std::endl;
+    return 0;
+}
+
+void other_stuff()
+{
+}
